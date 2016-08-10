1788564702f16ca6ced3a83efc4bd6c19e182bea
Justin Hinerman <hinerman@gmail.com>
2016-08-10 20:58:44 +0000

    Complete implementation

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require("express");
+var moment = require("moment");
 
 var app = express();
 
-app.get("/", function(req, res) {
-   res.send("Hello world!"); 
+app.get("/:input", function(req, res) {
+   console.log("got request: " + req.params.input);
+   var dateInput = req.params.input;
+   var response = "";
+   
+   var dateMoment = moment(new Date(dateInput));
+   if (dateMoment.isValid()) {
+       response = {
+            "unix": dateMoment.format("X"), 
+            "natural": dateMoment.format("MMMM DD, YYYY")
+       }
+       res.json(response);
+   } else {
+       res.json({
+          "unix":null,
+          "natural":null
+       });
+   }
 });
 
 app.listen(8080, function() {
-    console.log("Example app listening on port 8080!");
-})
+    console.log("Listening on port 8080!");
+});
